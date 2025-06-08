94670da1db4e873a28a7bbb74c106f7f14ca4885
Oussama <Messamessus@yahoo.com>
2025-06-08 12:05:13 +0100

    update user dash res

--- src/pages/products/Addings to OrderPage.jsx
+++
@@ -1,7 +0,0 @@
-Would you also like me to:
-
-Add a “Status” badge to each order (like En attente, Expédiée, etc.)?
-
-Add smooth entry animations to orders/products?
-
-Translate all text from ordersPage.xx if needed?
