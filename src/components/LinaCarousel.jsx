a8459c1682e24f96d93e4a9858462ca20fe96a17
SalahLaouini <linaopticlunettes@gmail.com>
2025-07-06 18:36:33 +0100

    Update Lina Carousel

--- src/components/LinaCarousel.jsx
+++ src/components/LinaCarousel.jsx
@@ -4,9 +4,9 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // 📦 Carousel
 import "../Styles/StylesLinaCarousel.css";
 
 // 📷 Carousel images
-import CarreraImg from "../assets/lina-carousel-lunettes/carrera.jpg";
-import CerrutiImg from "../assets/lina-carousel-lunettes/cerruti.jpg";
-import GucciImg from "../assets/lina-carousel-lunettes/gucci.jpg";
+import CarreraImg from "../assets/lina-carousel/carrera.jpg";
+import CerrutiImg from "../assets/lina-carousel/cerruti.jpg";
+import GucciImg from "../assets/lina-carousel/gucci.jpg";
 
 const LinaCarousel = () => {
   // 🖱️ Custom cursor animation inside the carousel wrapper
